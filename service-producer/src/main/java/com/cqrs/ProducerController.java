eca6f8d357edaded18db15771ccf1c294e44c4c6
shreyk2 <108486158+shreyk2@users.noreply.github.com>
2024-07-11 12:36:26 +0800

    Completed PR review changes

--- service-producer/src/main/java/com/cqrs/ProducerController.java
+++ service-producer/src/main/java/com/cqrs/ProducerController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Producer1Controller {
+public class ProducerController {
     @GetMapping("/produce")
     public String produce(){
-        return "Message from producer 1";
+        return "Message from producer";
     }
 
 }
--- service-producer/src/main/java/com/cqrs/ServiceProducerApplication.java
+++ service-producer/src/main/java/com/cqrs/ServiceProducerApplication.java
@@ -2,14 +2,14 @@ package com.cqrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
-public class ServiceProducer1Application {
+public class ServiceProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceProducer1Application.class, args);
+		SpringApplication.run(ServiceProducerApplication.class, args);
 	}
 
 }
