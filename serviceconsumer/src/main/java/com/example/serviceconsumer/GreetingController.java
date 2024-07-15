37fc76e298f9a2160a9c60f313e753ad9a0b3a80
AkshayV777 <akhi.allstar@gmail.com>
2024-07-15 11:16:14 +0800

    Completed Ticket-Create a service consumer

--- serviceconsumer/src/main/java/com/example/serviceconsumer/GreetingController.java
+++ serviceconsumer/src/main/java/com/example/serviceconsumer/GreetingController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
-@RequestMapping("/products")
 public class GreetingController {
  
     @Autowired
