20cc42e0c64669c8b77c01988bf9d90b10f5ae57
Hannibal Han <hannibal.han@taodigitalusa.com>
2024-07-21 21:52:19 +0800

    integrated Kafka producer and consumer.

--- service-producer/src/main/java/com/cqrs/service/KafkaService.java
+++ service-producer/src/main/java/com/cqrs/service/KafkaService.java
@@ -1,20 +1,30 @@
 package com.cqrs.service;
 
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class KafkaService {
 
+  private final KafkaTemplate<String, String> template;
+
   @KafkaListener(id="commandListener", topics = "command-topic")
-  @SendTo // use default replyTo expression
-  public String commandListener(String in) {
-    log.info("Command Listener received: {}", in);
-    return in.toUpperCase();
+  public void commandListener(@Payload String in, @Header(KafkaHeaders.CORRELATION_ID) byte[] cid, @Header(KafkaHeaders.REPLY_TOPIC) String topic) {
+    log.info("Command Listener received: payload {}, cid {}, topic {}", in, cid, topic);
+    var record = new ProducerRecord<String, String>(topic, in.toUpperCase());
+    record.headers().add(KafkaHeaders.CORRELATION_ID, cid);
+    template.send(record);
   }
 
   @KafkaListener(id="queryListener", topics = "query-topic")
